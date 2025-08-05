f27aa301ed1be03a661a192f1e8b57c44ee28432
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-08-05 17:05:56 +0200

    Add ability to view individual messages
    This commit adds the functionality to view an individual message by
    assigning each one a randomUUID, and passing that as the parameter to
    the router.

--- routes/router.js
+++ routes/router.js
@@ -1,14 +1,17 @@
 const Router = require("express");
+const { randomUUID } = require("node:crypto");
 
 const router = Router();
 
 const messages = [
     {
+        id: randomUUID(),
         text: "Hi there!",
         user: "Amanda",
         added: new Date(),
     },
     {
+        id: randomUUID(),
         text: "Hello World!",
         user: "Olivia",
         added: new Date(),
@@ -19,11 +22,26 @@ router.get("/", (req, res) => {
     res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 router.get("/new", (req, res) => {
-    res.render("form");
+    res.render("form", { title: "New Message" });
 });
+router.get("/:messageId", (req, res) => {
+    const { messageId } = req.params;
+    const message = messages.find((msg) => msg.id === messageId);
+    if (!message) {
+        return res.status(404).send("Message not found");
+    }
+
+    res.render("message", { title: "View Message", message });
+});
+
 router.post("/new", (req, res) => {
     const { text, user } = req.body;
-    messages.push({ text: text, user: user, added: new Date() });
+    messages.push({
+        id: randomUUID(),
+        text: text,
+        user: user,
+        added: new Date(),
+    });
     console.log(messages);
     res.redirect("/");
 });
