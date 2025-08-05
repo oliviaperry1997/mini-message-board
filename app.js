8a9fb8904e8f7c0dc27012b17f12a5a492a7ed31
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-08-05 14:05:13 +0200

    Add new message form with post functionality
    This commit adds a page for writing a new message, and the functionality
    for posting that message.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const router = require("./routes/router");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
 
 const PORT = 3000;
--- routes/router.js
+++ routes/router.js
@@ -18,5 +18,15 @@ const messages = [
 router.get("/", (req, res) => {
     res.render("index", { title: "Mini Messageboard", messages: messages });
 });
+router.get("/new", (req, res) => {
+    res.render("form");
+});
+router.post("/new", (req, res) => {
+    const { text, user } = req.body;
+    messages.push({ text: text, user: user, added: new Date() });
+    console.log(messages);
+    res.send("Form submitted successfully!");
+    res.redirect("/");
+});
 
 module.exports = router;
