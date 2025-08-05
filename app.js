9b8f0fe9d0430514a5b452f33522fc8e013fbb78
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-08-05 12:58:59 +0200

    Create sample messages and add message rendering
    This commit adds a couple sample messages and implements dynamic message
    rendering using res.locals.

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amanda",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Olivia",
+        added: new Date(),
+    },
+];
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 const PORT = 3000;
